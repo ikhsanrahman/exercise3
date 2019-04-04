0a7126ec8a2000122be4aac25e7776bff42aa960
ikhsanrahman <ikhsanrahman.h@gmail.com>
2019-04-05 01:52:29 +0700

    updated

--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ request.onload = function () {
     // console.log(request.responseText)
   
     var data = JSON.parse(this.response);
-    
+
 
     if (request.status >= 200 & request.status < 400) {
         data.results.forEach(output => {
@@ -29,73 +29,45 @@ request.onload = function () {
             // const h4 = document.createElement("h4");
             // h4.textContent = 1;
             
-            const col1 = document.createElement('td')
-            col1.textContent = "name"
             const baris1 = document.createElement("td");
             baris1.textContent = output.name;
 
-            const col2 = document.createElement('td')
-            col2.textContent = "rotation_period"
             const baris2 = document.createElement("td");
             baris2.textContent = output.rotation_period;
 
-            const col3 = document.createElement('td')
-            col3.textContent = "orbital_period"
             const baris3 = document.createElement("td");
             baris3.textContent = output.orbital_period;
 
-            const col4 = document.createElement('td')
-            col4.textContent = "diameter"
             const baris4 = document.createElement("td");
             baris4.textContent = output.diameter;
 
-            const col5 = document.createElement('td')
-            col5.textContent = "climate"
             const baris5 = document.createElement("td");
             baris5.textContent = output.climate;
 
-            const col6 = document.createElement('td')
-            col6.textContent = "gravity"
             const baris6 = document.createElement("td");
             baris6.textContent = output.gravity;
 
-            const col7 = document.createElement('td')
-            col7.textContent = "terrain"
             const baris7 = document.createElement("td");
             baris7.textContent = output.terrain;
 
-            const col8 = document.createElement('td')
-            col8.textContent = "surface_water"
             const baris8 = document.createElement("td");
             baris8.textContent = output.surface_water;
 
-            const col9 = document.createElement('td')
-            col9.textContent = "population"
             const baris9 = document.createElement("td");
             baris9.textContent = output.population;
 
-            const col10 = document.createElement('td')
-            col10.textContent = "residents"
             const baris10 = document.createElement("td");
             baris10.textContent = output.residents;
 
-            const col11 = document.createElement('td')
-            col11.textContent = "films"
             const baris11 = document.createElement("td");
             baris11.textContent = output.films;
 
-            const col12 = document.createElement('td')
-            col12.textContent = "created_at"
             const baris12 = document.createElement("td");
             baris12.textContent = output.created;
 
-            const col13 = document.createElement('td')
-            col13.textContent = "edited"
             const baris13 = document.createElement("td");
             baris13.textContent = output.edited;
 
-            const col14 = document.createElement('td')
-            col14.textContent = "url"
             const baris14 = document.createElement("td");
             baris14.textContent = output.url;
 
@@ -104,20 +76,6 @@ request.onload = function () {
 
             container.appendChild(table);
             // table.appendChild(h4);
-            // table.appendChild(col1);
-            // table.appendChild(col2);
-            // table.appendChild(col3);
-            // table.appendChild(col4);
-            // table.appendChild(col5);
-            // table.appendChild(col6);
-            // table.appendChild(col7);
-            // table.appendChild(col8);
-            // table.appendChild(col9);
-            // table.appendChild(col10);
-            // table.appendChild(col11);
-            // table.appendChild(col12);
-            // table.appendChild(col13);
-            // table.appendChild(col14);
             table.appendChild(baris1);
             table.appendChild(baris2);
             table.appendChild(baris3);
